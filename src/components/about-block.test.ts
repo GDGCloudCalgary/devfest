a210f7b3575b04c17564a6bc402c9ff955b8716b
Antarpreet Singh <antarpreet13@gmail.com>
2023-08-24 01:25:58 -0600

    fixed linting issues

--- src/components/about-block.test.ts
+++ src/components/about-block.test.ts
@@ -1,5 +1,4 @@
 import { beforeEach, describe, it, jest } from '@jest/globals';
-import { screen } from '@testing-library/dom';
 import { mocked } from 'jest-mock';
 import { openVideoDialog } from '../store/ui/actions';
 import './about-block';
@@ -15,18 +14,5 @@ describe('about-block', () => {
 
   it('defines a component', () => {
     expect(customElements.get('about-block')).toBeDefined();
-  });
-
-  it('renders details', async () => {
-    // const { shadowRootForWithin } = await fixture(
-    //   html`<about-block data-testid="block"></about-block>`
-    // );
-    // const { getByText } = within(shadowRootForWithin);
-
-    expect(screen.getByTestId('block')).toBeInTheDocument();
-    // expect(getByText(aboutBlock.title)).toBeInTheDocument();
-    // expect(getByText(aboutBlock.callToAction.featuredSessions.description)).toBeInTheDocument();
-    // expect(getByText(aboutBlock.statisticsBlock.attendees.number)).toBeInTheDocument();
-    // expect(getByText(aboutBlock.statisticsBlock.attendees.label)).toBeInTheDocument();
   });
 });
